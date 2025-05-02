28786dc1bf6b0981d69ab8ad082a8bef5df5a603
Julian Andres robayo Quintero <jar@udistrital.edu.co>
2025-05-01 23:17:55 -0500

    v 0.5 Mapa y Logo

--- docs/programa.js
+++ docs/programa.js
@@ -1,1 +1,4 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([4.693154616794554, -74.11102194695583], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
