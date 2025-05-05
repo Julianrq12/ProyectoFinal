f79b1fd166adc0789560746032946707ce49c485
Julián Andrés Robayo Quintero <jar@udistrital.edu.co>
2025-05-05 15:18:29 -0500

    v 0.5

--- docs/programa.js
+++ docs/programa.js
@@ -2,3 +2,8 @@ var map = L.map('map').setView([4.693154616794554, -74.11102194695583], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+let MiObjeto= document.getElementById("MiEtiqueta")
+for(let y=0; y=1e6; y++){
+    MiObjeto.innerHTML=MiObjeto.innerHTML+y
+}
