afbea8541ea8c8c6e120bcc7a0656f4becd1d0be
Julián Andrés Robayo Quintero <jar@udistrital.edu.co>
2025-05-21 15:51:21 -0500

    v 0.7 Acerca de, descripcion, autor

--- docs/programa.js
+++ docs/programa.js
@@ -10,3 +10,39 @@
         function onMapClick(e){
             alert("Posición: "+e.latlng)
         }
+
+        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+const path = window.location.pathname;
+
+if (path.includes("acerca.html")) {
+    var polygon = L.polygon([
+        [4.698233311980569, -74.1148684543079],
+        [4.693593669420297, -74.10734424011537],
+        [4.690015009317711, -74.10909447698988],
+        [4.694672658815977, -74.11712391419775]
+    ], {
+        color: 'green',
+        fillColor: '#9fdfbf',
+        fillOpacity: 0.4
+    }).addTo(map).bindPopup("Zona de monitoreo");
+    map.fitBounds(polygon.getBounds());
+
+} else if (path.includes("autor.html")) {
+    // marcador para página del autor
+    map.setView([4.603851294991477, -74.08654541312458], 16);
+    L.marker([4.603851294991477, -74.08654541312458]).addTo(map)
+        .bindPopup("Ubicación del autor");
+} else {
+    // marcador general por defecto
+    L.marker([4.693154616794554, -74.11102194695583]).addTo(map)
+        .bindPopup("Barrio La Florida");
+}
