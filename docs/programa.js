ace0e35f2762e78d9a46e412b5d8a2ad048e30b6
Julian Andres robayo Quintero <jar@udistrital.edu.co>
2025-05-21 14:46:38 -0500

    v 0.6 Pagina Base

--- docs/programa.js
+++ docs/programa.js
@@ -1,4 +1,12 @@
-var map = L.map('map').setView([4.693154616794554, -74.11102194695583], 13);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+        var map = L.map('map').setView([4.693154616794554, -74.11102194695583], 16);
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom: 25,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map); 
+
+        L.marker([4.693154616794554, -74.11102194695583]).addTo(map).bindPopup("Barrio La Florida")
+
+        map.on('click', onMapClick)
+        function onMapClick(e){
+            alert("Posición: "+e.latlng)
+        }
