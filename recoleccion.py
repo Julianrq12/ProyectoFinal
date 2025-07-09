08af0a2bfa894509ae74f2494506075171c1b6a8
Julian Andres Robayo Quintero <jar@udistrital.edu.co>
2025-07-09 09:31:02 -0500

    Base de datos Terminada

--- recoleccion.py
+++ recoleccion.py
@@ -1,12 +1,11 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 import psycopg2
 
 app = Flask(__name__)
 CORS(app)
 
-# Conexión a PostgreSQL
-def obtener_conexion():
+def get_db_connection():
     return psycopg2.connect(
         dbname='recoleccion',
         user='postgres',
@@ -15,36 +14,46 @@ def obtener_conexion():
         port='5432'
     )
 
-@app.route('/')
-def inicio():
-    return render_template('index.html')
-
-@app.route('/anadir')
-def anadir():
-    return render_template('anadir.html')
-
 @app.route('/guardar', methods=['POST'])
 def guardar():
-    data = request.json
-    nombre = data.get('nombre')
-    contacto = data.get('contacto')
-    tipo = data.get('tipo')
-    descripcion = data.get('descripcion')
-    direccion = data.get('direccion')
-    lat = data.get('latitud')
-    lng = data.get('longitud')
-
-    conn = obtener_conexion()
+    data = request.get_json()
+    conn = get_db_connection()
     cur = conn.cursor()
     cur.execute("""
-        INSERT INTO reportes (nombre, contacto, tipo, descripcion, direccion, latitud, longitud)
-        VALUES (%s, %s, %s, %s, %s, %s, %s)
-    """, (nombre, contacto, tipo, descripcion, direccion, lat, lng))
+        INSERT INTO reportes (nombre, contacto, direccion, tipo_reporte, descripcion)
+        VALUES (%s, %s, %s, %s, %s)
+    """, (
+        data['nombre'],
+        data['contacto'],
+        data['direccion'],
+        data['tipo'],
+        data['descripcion']
+    ))
     conn.commit()
     cur.close()
     conn.close()
+    return jsonify({'mensaje': 'Datos guardados correctamente'}), 200
 
-    return jsonify({"mensaje": "Datos guardados exitosamente"})
+@app.route('/datos', methods=['GET'])
+def obtener_datos():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM reportes ORDER BY id DESC")
+    rows = cur.fetchall()
+    cur.close()
+    conn.close()
+
+    reportes = []
+    for row in rows:
+        reportes.append({
+            'id': row[0],
+            'nombre': row[1],
+            'contacto': row[2],
+            'direccion': row[3],
+            'tipo_reporte': row[4],
+            'descripcion': row[5]
+        })
+    return jsonify(reportes)
 
 if __name__ == '__main__':
     app.run(debug=True)
